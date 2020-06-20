265991da51f5345a236488dced22612931284065
HangHuynh98 <thuhang110998@gmail.com>
2020-06-20 14:53:33 +0700

    fix loginAdmin/loginUser

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 router.post("/login", require("./login"));
+router.post("/loginAdmin", require("./loginAdmin"));
 router.post("/register", require("./register"));
 router.get("/auth",require("./auth"));
 
