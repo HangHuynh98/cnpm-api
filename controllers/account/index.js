35812dc91b8b8f633229acd4094d6b441eb5c49e
HangHuynh98 <thuhang110998@gmail.com>
2020-06-23 01:30:26 +0700

    change link and fix role

--- controllers/account/index.js
+++ controllers/account/index.js
@@ -6,10 +6,10 @@ const {checkRoleUser,checkRoleAdmin } = require('../../middlewares/role')
 
 adminRouter.patch("/changeStatusUser/:id", requiredAdmin,checkRoleUser, checkRole, require("./changeStatusAccount"));
 adminRouter.patch("/changeStatusAdmin/:id", requiredAdmin,checkRoleAdmin, checkRole, require("./changeStatusAccount"));
-adminRouter.get("/listUser", requiredAdmin,checkRoleUser, checkRole, require("./getListUser"));
+adminRouter.get("/listUsers", requiredAdmin,checkRoleUser, checkRole, require("./getListUser"));
 adminRouter.get("/getUser/:id",requiredAdmin,checkRoleUser, checkRole,  require("./getAccountById"));
 adminRouter.get("/getAdmin/:id",requiredAdmin,checkRoleAdmin, checkRole,  require("./getAccountById"));
-adminRouter.get("/listAdmin",requiredAdmin,checkRoleAdmin, checkRole,require("./getListAdmin"));
+adminRouter.get("/listAdmins",requiredAdmin,checkRoleAdmin, checkRole,require("./getListAdmin"));
 adminRouter.patch("/changeRole/:id", requiredAdmin,checkRoleAdmin, checkRole, require("./changeRole"));
 
 router.patch("/changePassword", requiredLogin, require("./changePassword"));
